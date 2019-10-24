02198d6df7acfb4ea0bd3ef7bcf586ca32a89c18
Igor Trindade <igorhideki@gmail.com>
2019-10-24 06:12:32 -0400

    add layout da tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,53 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} style={{ width: 54, height: 54 }} />
+
+        <Form>
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Sua senha secreta"
+            returnKeyType="send"
+            ref={passwordRef}
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton>Entrar</SubmitButton>
+        </Form>
+
+        <SignLink>
+          <SignLinkText>Criar conta grátis</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 }
