0124764071891771fefb6f1853d24ad882229ef1
Igor Trindade <igorhideki@gmail.com>
2019-10-25 06:04:47 -0400

    add layout de SignUp

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,68 @@
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+
+import Background from '~/components/Background';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+import logo from '~/assets/logo.png';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} style={{ width: 48, height: 48 }} />
+
+        <Form>
+          <FormInput
+            autoCorrect={false}
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+
+          <FormInput
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Sua senha secreta"
+            returnKeyType="send"
+            ref={passwordRef}
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton>Criar conta</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho login</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
