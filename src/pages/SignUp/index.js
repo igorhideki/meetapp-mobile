b7567c62da157640fe938a9e0bf1a87c8637ede0
Igor Trindade <igorhideki@gmail.com>
2019-10-26 07:56:34 -0400

    add chamada requisicao para fazer cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
 import Background from '~/components/Background';
 import {
   Container,
@@ -14,10 +16,17 @@ import {
 import logo from '~/assets/logo.png';
 
 export default function SignUp({ navigation }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const emailRef = useRef();
   const passwordRef = useRef();
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -30,6 +39,8 @@ export default function SignUp({ navigation }) {
             placeholder="Nome completo"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
 
           <FormInput
@@ -39,18 +50,25 @@ export default function SignUp({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
+            secureTextEntry
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Sua senha secreta"
             returnKeyType="send"
             ref={passwordRef}
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton>Criar conta</SubmitButton>
+          <SubmitButton onPress={handleSubmit} loading={loading}>
+            Criar conta
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
