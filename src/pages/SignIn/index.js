0723fccf59ea22bc10a39ace26fd9d9ffa15369f
Igor Trindade <igorhideki@gmail.com>
2019-10-26 07:56:23 -0400

    add chamada requisicao para fazer login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 
+import { signInRequest } from '~/store/modules/auth/actions';
 import Background from '~/components/Background';
 import {
   Container,
@@ -14,9 +16,15 @@ import {
 import logo from '~/assets/logo.png';
 
 export default function SignIn({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const passwordRef = useRef();
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -31,18 +39,25 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
+            secureTextEntry
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Sua senha secreta"
             returnKeyType="send"
             ref={passwordRef}
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton>Entrar</SubmitButton>
+          <SubmitButton onPress={handleSubmit} loading={loading}>
+            Entrar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignUp')}>
