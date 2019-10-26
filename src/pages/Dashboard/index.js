e3a30c25a868dc686b800098e0f7077053b10425
Igor Trindade <igorhideki@gmail.com>
2019-10-26 15:28:40 -0400

    add lista de meetups na dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,130 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format, subDays, addDays, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
 import Header from '~/components/Header';
-import { Container } from './styles';
+import Meetup from '~/components/Meetup';
+import Button from '~/components/Button';
+import {
+  Container,
+  DateSelector,
+  PrevButton,
+  NextButton,
+  DateText,
+  List,
+  Helper,
+  HelperText,
+  Loading,
+} from './styles';
 
 export default function Dashboard() {
+  const [meetups, setMeetups] = useState([]);
+  const [date, setDate] = useState(new Date());
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasNoMoreMeetups, setHasNoMoreMeetups] = useState(false);
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadMeetups() {
+      setLoading(true);
+
+      const response = await api.get('meetups', { params: { date, page: 1 } });
+
+      setLoading(false);
+
+      if (response.data.length) {
+        const data = response.data.map(meetup => {
+          return {
+            ...meetup,
+            dateFormatted: format(
+              parseISO(meetup.date),
+              "d 'de' MMMM, 'às' H'h'",
+              {
+                locale: pt,
+              }
+            ),
+          };
+        });
+
+        setMeetups([data]);
+      } else {
+        setHasNoMoreMeetups(true);
+      }
+    }
+
+    loadMeetups();
+  }, [date]);
+
+  useEffect(() => {
+    setMeetups([]);
+    setHasNoMoreMeetups(false);
+    setPage(1);
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  function loadMoreMeetups() {
+    if (hasNoMoreMeetups || loading) return;
+
+    setPage(page + 1);
+
+    console.tron.log(page);
+  }
+
   return (
     <Background>
       <Container>
         <Header />
-        <Text>Dashboard</Text>
+
+        <DateSelector>
+          <PrevButton onPress={handlePrevDay}>
+            <Icon name="chevron-left" size={30} color="#fff" />
+          </PrevButton>
+          <DateText>{dateFormatted}</DateText>
+          <NextButton onPress={handleNextDay}>
+            <Icon name="chevron-right" size={30} color="#fff" />
+          </NextButton>
+        </DateSelector>
+
+        {meetups.length ? (
+          <List
+            data={meetups}
+            keyExtractor={meetup => String(meetup.id)}
+            onEndReachedThreshold={0.1}
+            onEndReached={loadMoreMeetups}
+            renderItem={({ item }) => (
+              <Meetup data={item}>
+                {!item.past && (
+                  <Button onPress={() => {}}>Realizar inscrição</Button>
+                )}
+              </Meetup>
+            )}
+          />
+        ) : (
+          <Helper>
+            <HelperText>Ainda não há meetups para esta data.</HelperText>
+          </Helper>
+        )}
+
+        {loading && (
+          <Loading>
+            <ActivityIndicator />
+          </Loading>
+        )}
       </Container>
     </Background>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,3 +3,42 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const DateSelector = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0;
+`;
+
+export const PrevButton = styled.TouchableOpacity``;
+
+export const NextButton = styled.TouchableOpacity``;
+
+export const DateText = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 20px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 20 },
+})``;
+
+export const Helper = styled.View`
+  padding: 20px;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HelperText = styled.Text`
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const Loading = styled.View`
+  margin: 20px 0;
+`;
